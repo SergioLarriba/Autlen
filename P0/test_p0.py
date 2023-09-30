1031741456321329a1fc5470414f8afee94c3152
Sergio Larriba Moreno <98891869+SergioLarriba@users.noreply.github.com>
2023-09-30 17:30:32 +0200

    Revisar el 5

--- P0/test_p0.py
+++ P0/test_p0.py
@@ -13,13 +13,14 @@ class TestP0(unittest.TestCase):
         with self.subTest(string=string):
             match = re.fullmatch(expr, string)
             self.assertEqual(bool(match), expected)
-
+    
     def test_exercise_0(self) -> None:
-        self.check_expression(RE0, "", True)
         self.check_expression(RE0, "a", True)
         self.check_expression(RE0, "bbbbaba", True)
+
         self.check_expression(RE0, "abbab", False)
         self.check_expression(RE0, "b", False)
+        self.check_expression(RE0, "", False)
 
     def test_exercise_1(self) -> None:
         self.check_expression(RE1, "", True)
@@ -39,7 +40,7 @@ class TestP0(unittest.TestCase):
         self.check_expression(RE2, "bb", False)
         self.check_expression(RE2, "aaa", False)
         self.check_expression(RE2, "", False)
-    """
+
     def test_exercise_3(self) -> None:
         self.check_expression(RE3, "aabb", True)
         self.check_expression(RE3, "abbaa", True)
@@ -48,6 +49,7 @@ class TestP0(unittest.TestCase):
         self.check_expression(RE3, "ababaa", False)
         self.check_expression(RE3, "abbabb", False)
         self.check_expression(RE3, "bba", False) 
+        self.check_expression(RE3, "", False) 
     
     def test_exercise_4(self) -> None:
         self.check_expression(RE4, "0", True)
@@ -58,6 +60,17 @@ class TestP0(unittest.TestCase):
         self.check_expression(RE4, "3.33", False)
         self.check_expression(RE4, "256", False)
 
+
+        self.check_expression(RE4, "", False) #rechazamos la cadena vacia
+        self.check_expression(RE4, "007", False) #rechaza ceros innecesarios a la izquierda
+        self.check_expression(RE4, "017", False) #rechaza ceros innecesarios a la izquierda
+        self.check_expression(RE4, "/(*", False) #rechaza simbolos que nos estan en el alfabeto
+        self.check_expression(RE4, "+1", False)
+        for i in range(0, 255):
+                self.check_expression(RE4, str(i), True)	
+        for i in range(256, 2000):
+                self.check_expression(RE4, str(i), False)
+    
     def test_exercise_5(self) -> None:
         self.check_expression(RE5, "a", True)
         self.check_expression(RE5, "aa", True)
@@ -70,15 +83,28 @@ class TestP0(unittest.TestCase):
         self.check_expression(RE5, "bbaabaabababaa", False)
     
     def test_exercise_6(self) -> None:
-        #self.check_expression(RE6, "", True)
+        self.check_expression(RE6, "", True)
         self.check_expression(RE6, "1", True)
         self.check_expression(RE6, "0", True)
         self.check_expression(RE6, "0011", True)
         self.check_expression(RE6, "110", True)
+        self.check_expression(RE6, "11010101", True)
+        self.check_expression(RE6, "011001010", True)
+        self.check_expression(RE6, "01100", True)
+        self.check_expression(RE6, "0100101101", True)
+        self.check_expression(RE6, "10110100", True)
+
 
         self.check_expression(RE6, "001011011101",False)
+        self.check_expression(RE6, "000",False)
+        self.check_expression(RE6, "111",False)
         self.check_expression(RE6, "0000", False)
         self.check_expression(RE6, "1111", False)
-    """
+        self.check_expression(RE6, "0101010110101001011", False)
+        self.check_expression(RE6, "101101000", False)
+        self.check_expression(RE6, "10110100100", False)
+        
+    
+    
 if __name__ == '__main__':
     unittest.main()
