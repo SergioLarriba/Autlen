ec1a79fdc3243e5a3933174ec3f6b571f74bf21f
Sergio Larriba Moreno <98891869+SergioLarriba@users.noreply.github.com>
2023-10-13 18:11:39 +0200

    Add files via upload

--- P1/automata/automaton_evaluator.py
+++ P1/automata/automaton_evaluator.py
@@ -34,14 +34,25 @@ class FiniteAutomatonEvaluator():
     def process_symbol(self, symbol):
         """
         Process one symbol.
-
         Args:
             symbol: Symbol to consume. Type: str
 
         """
-        self.current_states = self.automaton.transitions.get_transition(self.current_states, symbol)
-        self.current_states = self._complete_lambdas(self.current_states)
+        #self.current_states = self.automaton.transitions.get_transition(self.current_states, symbol)
+        #self.current_states = self._complete_lambdas(self.current_states)
+        # Control de errores 
+        if symbol not in self.automaton.symbols:
+            raise ValueError("The symbol is not in the alphabet of the automaton")
+        
+        estados_nuevos = set()
+        for state in self.current_states:
+            estados_nuevos.update(self.automaton.get_transition(state,symbol))
 
+        self._complete_lambdas(estados_nuevos)
+        self.current_states = estados_nuevos
+        
+        return 
+        
         
 
         
@@ -49,28 +60,15 @@ class FiniteAutomatonEvaluator():
         """
         Add states reachable with lambda transitions to the set.
 
+        {State('Empty', is_final=False)}
+        {State('1', is_final=False)}
+
         Args:
             set_to_complete: Current set of states to be completed.
         """
-        #---------------------------------------------------------------------
-        
-        """"Añade al conjunto de estados pasado como
-        argumento todos los estados que sean alcanzables mediante un número
-        arbitrario de transiciones lambda"""
-        
-        #---------------------------------------------------------------------
-        # TO DO:
-        # add usefull code if necessary
-        #---------------------------------------------------------------------
-        new_states = set()
-        for state in set_to_complete:
-            new_states = new_states.union(self.automaton.transitions.get_transition(state, ''))
-        if new_states == set():
-            return set_to_complete
-        else:
-            return set_to_complete.union(self._complete_lambdas(new_states))
-    
+         
 
+        
         
     def process_string(self, string):
         """
@@ -87,10 +85,13 @@ class FiniteAutomatonEvaluator():
     def is_accepting(self):
         """Check if the current state is an accepting one."""
         
-        return self.current_states.intersection(self.automaton.accepting_states) != set()
+        for state in self.current_states: 
+            if state.is_final: 
+                return True 
+        
+        return False 
     
         
-
     def accepts(self, string):
         """
         Return if a string is accepted without changing state.
