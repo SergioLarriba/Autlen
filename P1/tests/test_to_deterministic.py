f4f69496bcd584aceb0d94c62ff8088f1b69f731
Sergio Larriba Moreno <98891869+SergioLarriba@users.noreply.github.com>
2023-11-06 13:48:35 +0100

    Ej 3 + tests hecho

--- P1/tests/test_to_deterministic.py
+++ P1/tests/test_to_deterministic.py
@@ -51,7 +51,7 @@ class TestTransform(ABC, unittest.TestCase):
         expected = AutomataFormat.read(expected_str)
 
         self._check_transform(automaton, expected)
-    #Tests para comprobar lambdas
+    
     def test_case2(self):
         """Test Case 2"""
         automaton_str = """
@@ -132,6 +132,145 @@ class TestTransform(ABC, unittest.TestCase):
 
         self._check_transform(automaton, expected)
 
+    def test_case3(self):
+        """Test Case 3."""
+        automaton_str = """
+        Automaton:
+        Symbols: ab
+        
+        q0
+        q1
+        q2
+        q3
+        q4 final
+        
+        ini q0 -a-> q0
+        ini q0 -b-> q0
+        ini q0 -b-> q1
+
+        q1 -a-> q1
+        q1 -b-> q0
+        q1 -b-> q2
+        
+        q2 -a-> q3
+        q2 -b-> q2
+
+        q3 -a-> q3
+        q3 -a-> q4
+        q3 -b-> q3
+
+        q4 -a-> q0
+        q4 -b-> q0 
+        """
+
+        automaton = AutomataFormat.read(automaton_str)
+
+        expected_str = """
+        Automaton:
+        Symbols: ab
+        
+        q0
+        q0q1
+        q0q1q2
+        q0q1q3
+        q0q1q2q3
+        q0q1q3q4 final 
+        empty 
+        
+        ini q0 -a-> q0
+        ini q0 -b-> q0q1
+        
+        q0q1 -a-> q0q1
+        q0q1 -b-> q0q1q2 
+
+        q0q1q2 -b-> q0q1q2
+        q0q1q2 -a-> q0q1q3
+
+        q0q1q3 -b-> q0q1q2q3
+        q0q1q3 -a-> q0q1q3q4
+
+        q0q1q2q3 -b-> q0q1q2q3
+        q0q1q2q3 -a-> q0q1q3q4
+
+        q0q1q3q4 -a-> q0q1q3q4
+        q0q1q3q4 -b-> q0q1q2q3 
+
+        empty -a-> empty
+        empty -b-> empty
+        """
+
+        expected = AutomataFormat.read(expected_str)
+
+        self._check_transform(automaton, expected)
+
+    def test_case4(self):
+        """Test Case 4."""
+        automaton_str = """
+        Automaton:
+        Symbols: 01
+        
+        q0
+        q1
+        q2
+        q3
+        q4
+        q5
+        q6 final 
+        q7
+        
+        ini q0 --> q2
+        ini q0 --> q1
+
+        q1 -1-> q5
+        q1 -1-> q3
+
+        q2 -0-> q4 
+
+        q4 -1-> q6
+
+        q5 --> q7
+        q5 -0-> q4
+        q5 -0-> q3 
+
+        q7 --> q6 
+        """
+
+        automaton = AutomataFormat.read(automaton_str)
+
+        expected_str = """
+        Automaton:
+        Symbols: 01
+        
+        q0q1q2
+        q4
+        q3q5q6q7 final
+        q3q4
+        q6 final 
+        empty
+        
+        ini q0q1q2 -0-> q4
+        ini q0q1q2 -1-> q3q5q6q7
+        
+        q4 -1-> q6
+        q4 -0-> empty
+
+        q3q5q6q7 -0-> q3q4
+        q3q5q6q7 -1-> empty 
+
+        q3q4 -1-> q6
+        q3q4 -0-> empty
+
+        q6 -0-> empty
+        q6 -1-> empty
+
+        empty -0-> empty
+        empty -1-> empty
+        """
+
+        expected = AutomataFormat.read(expected_str)
+
+        self._check_transform(automaton, expected)
+    
     
 if __name__ == '__main__':
     unittest.main()
